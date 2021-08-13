e1e03fff254dd54b2994e356d40ea43027c284a5
Susanna Skornicka <susannaskornicka@gmail.com>
2021-08-13 17:07:53 -0400

    fixed problem with commas/add clear button

--- script.js
+++ script.js
@@ -14,51 +14,58 @@ let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", 
 let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-let mainArray = []
+let output = []
 let password = " "
 
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword) 
 
 function generatePassword() {
 
-  let passwordLength = prompt("How many charactors (between 8 and 16)");
-    if (!passwordLength || passwordLength <= 7 || passwordLength >=17) {
-      return alert("Please try again, invalid input. Entry must be between 8 and 16");
+  let passwordLength = prompt("How many charactors? (please choose a number between 8 and 128)");
+    if (!passwordLength || passwordLength <= 7 || passwordLength >=129) {
+      return alert("Please try again, invalid input. Entry must be between 8 and 128");
   }
 
   let addLowerCase = confirm("Do you want to add lowercase letters to your password?");
     if (addLowerCase === true) {
-      mainArray +=(lowerCase);
+      output = output.concat(lowerCase);
+    console.log(addLowerCase);
   }
 
-  let addUpperCase = confirm("Do you want to add uppercase letters to your password?");
+  let addUpperCase = confirm("Do you want to add UPPERCASE letters to your password?");
     if (addUpperCase === true) {
-      mainArray +=(upperCase);
+      output = output.concat(upperCase);
+    console.log(addUpperCase);
   }
 
   let addNumbers = confirm("Do you want to add numbers to your password?");
     if (addNumbers === true) {
-      mainArray +=(numbers);
+      output = output.concat(numbers);
+   console.log(addNumbers);
   }
 
   let addSpecialCharactors = confirm("Do you want to add special characters to your password?");
     if (addSpecialCharactors === true) {
-      mainArray +=(specialCharacters);
+      output = output.concat(specialCharacters);
+    console.log(addSpecialCharactors);
   }
 
   let password = " ";
     for (let i = 0; i < passwordLength; i++) {
-    password +=mainArray[Math.floor(Math.random() * (mainArray.length))];
-    console.log(Math.floor(Math.random() * (mainArray.length)));
-    console.log(password);
+      password = password +=output[Math.floor(Math.random(generatePassword) * (output.length))];
+    // console.log(Math.floor(Math.random(generatePassword) * (output.length)));
+    // console.log(password);
   }
-  
-return password;
+ 
+  return password;
 
 }
 
+function refreshPage(){
+   window.location.reload();
+ } 
 
 
 
