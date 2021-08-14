99a7576119f6dde1577d581204b9d1589edd535b
Susanna Skornicka <susannaskornicka@gmail.com>
2021-08-14 15:48:02 -0400

    final commit before due date

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # randomPasswordGenerator
+
+## Description
+
+This is a random password generator. It has alerts that pop up to inquire about desired password content: number of values needed, lower case, upper case, numbers and special charactors. There is a error alert for the requested number of values if outside of the paramater between 8 and 128. I added a 'clear' button to refresh the page to retry or make an additional password and reselctng the parameters. 
+
+## Usage
+
+This site allows individuals to generate random passwords based on selected criteria. This enhances the security of passwords utilized. 
+
+## Link
+
+https://su5ies.github.io/randomPasswordGenerator/
+
+## Screenshot
+
+![The Password Generator application displays a red button to "Generate Password" and alerts to compile password content.](./images/alerts.jpg) 
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// this  assigned the name generateBtn to the HTML object identified with the id=generate
+let generateBtn = document.querySelector("#generate");
+let clearBtn = document.querySelector("#clear");
 
-// Write password to the #password input
+// write the password with this function and display in browser
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
+// arrays to use to create passwords
 let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "~", "?", "<", ">", "/"];
 let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,55 +17,58 @@ let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let output = []
 let password = " "
 
-
-
+// when generate button is clicked,  the writePassword function is called
 generateBtn.addEventListener("click", writePassword) 
+clearBtn.addEventListener("click", refreshPage)
+
+//this large function contains the alert messages and recieves input for what to include in the password.
 
 function generatePassword() {
 
+  // determines password length within parameters
   let passwordLength = prompt("How many charactors? (please choose a number between 8 and 128)");
-    if (!passwordLength || passwordLength <= 7 || passwordLength >=129) {
-      return alert("Please try again, invalid input. Entry must be between 8 and 128");
+    if (passwordLength <= 7 || passwordLength >=129) {
+      return alert("Please try again, invalid input. Entry must be a number between 8 and 128");
   }
-
+  //determines if lowercase letters are to be included, if true adds to output
   let addLowerCase = confirm("Do you want to add lowercase letters to your password?");
     if (addLowerCase === true) {
       output = output.concat(lowerCase);
-    console.log(addLowerCase);
   }
-
+  //determines if uppercase letters are to be used, if true adds to output
   let addUpperCase = confirm("Do you want to add UPPERCASE letters to your password?");
     if (addUpperCase === true) {
       output = output.concat(upperCase);
-    console.log(addUpperCase);
   }
 
+  
+  //determines if numbers are to be used, if true adds to output
   let addNumbers = confirm("Do you want to add numbers to your password?");
     if (addNumbers === true) {
       output = output.concat(numbers);
-   console.log(addNumbers);
   }
 
+  //determines if special characters are to be used, if true adds to output
   let addSpecialCharactors = confirm("Do you want to add special characters to your password?");
     if (addSpecialCharactors === true) {
       output = output.concat(specialCharacters);
-    console.log(addSpecialCharactors);
   }
 
+  // compiles random password
   let password = " ";
     for (let i = 0; i < passwordLength; i++) {
       password = password +=output[Math.floor(Math.random(generatePassword) * (output.length))];
-    // console.log(Math.floor(Math.random(generatePassword) * (output.length)));
-    // console.log(password);
   }
  
+  // returns value to screen 
   return password;
-
 }
 
-function refreshPage(){
+  // 
+function refreshPage() {
+
    window.location.reload();
- } 
+} 
 
 
 
